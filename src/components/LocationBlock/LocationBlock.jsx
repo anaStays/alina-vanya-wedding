5eee7dd3d3215ff9946a6fb1c4e5bedfda65c47f
anaStays <an_vorotilina@mail.ru>
2026-07-14 13:45:09 +0300

    update location

--- src/components/LocationBlock/LocationBlock.jsx
+++ src/components/LocationBlock/LocationBlock.jsx
@@ -1,11 +1,11 @@
 import styles from './LocationBlock.module.css'
-import location1 from '../../assets/location1.jpg'
-import location2 from '../../assets/location2.jpg'
+import location1 from '../../assets/location11.jpg'
+import location2 from '../../assets/location12.jpg'
 
 export default function LocationBlock() {
   const handleMapClick = () => {
     window.open(
-      'https://yandex.com/maps/org/farm_village/74039836796/?ll=39.255306%2C51.824713&mode=search&sll=39.253261%2C51.824796&sspn=0.013918%2C0.005082&text=%D1%81%D0%B5%D0%BB%D0%BE%20%D0%A7%D0%B5%D1%80%D1%82%D0%BE%D0%B2%D0%B8%D1%86%D1%8B%2C%20%D0%9C%D0%BE%D0%BB%D0%BE%D0%B4%D1%91%D0%B6%D0%BD%D0%B0%D1%8F%20%D1%83%D0%BB.%2C%2024%D0%91%20farm%20village&z=17.01',
+      'https://yandex.ru/maps/193/voronezh/?ll=39.077039%2C51.633978&mode=poi&poi%5Bpoint%5D=39.076972%2C51.633994&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D180464763356&z=14',
       '_blank'
     )
   }
@@ -25,9 +25,9 @@ export default function LocationBlock() {
         </div>
 
         <div className={styles.address}>
-          <p className={styles.venueName}>Farm&amp;Village</p>
+          <p className={styles.venueName}>Груши и яблоки</p>
           <p className={styles.venueAddress}>
-            село Чертовицы, Молодёжная ул., 24Б
+            СНТ Тихий Дон, Кленовая аллея, 6
           </p>
         </div>
 
